665852c458f87a9b5d6add1636b0993fb1fb208c
Anita <chaudhar2134@gmail.com>
2018-09-25 00:41:57 +0530

    first commit

---
+++ app/src/main/java/com/example/anu/button/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.anu.button;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Click on button",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
